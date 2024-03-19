27536647dc08dc2cc07cf14b4173b65b44e5d63a
YunhoJangg <163809333+YunhoJangg@users.noreply.github.com>
2024-03-20 03:34:52 +0900

    Update frog_rsp.py
    플레이어의 값을 처리하는 함수 추가

--- frog_rsp.py
+++ frog_rsp.py
@@ -1,9 +1,23 @@
-import random
+import random #random 모듈 임포트
 
 class CPU_RSP: #컴퓨터의 값 처리
   def __init__ (self):
-    self.choice_list = ['가위','바위','보']
+    self.choice_list = ['가위','바위','보'] #컴퓨터가 낼 세 개의 값의 리스트 설정
   
   def choice (self):
-    computer_choice = random.choice(self.choice_list)
-    return computer_choice
+    computer_choice = random.choice(self.choice_list) #랜덤으로 위의 리스트에 있는 값 세 개 중 하나 선택
+    return computer_choice #고른 값 반환
+
+class Player_RSP: #플레이어의 값 처리
+  def __init__ (self):
+    self.choice_list = ['가위','바위','보'] #플레이어가 낼 세 개의 값의 리스트 설정
+
+  def choice (self):
+    player_choice = random.choice(self.choice_list) #랜덤으로 위의 리스트에 있는 값 세 개 중 하나 선택
+    return player_choice #고른 값 반환
+
+class Main: #본 게임
+  def __init__ (self):
+    print("청개구리 가위, 바위, 보를 시작합니다.")
+    cpu = CPU_RSP()
+    player = Player_RSP()
