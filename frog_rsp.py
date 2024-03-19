2c6b733402009d956a550aa8903cc3066be477a1
YunhoJangg <163809333+YunhoJangg@users.noreply.github.com>
2024-03-20 03:21:10 +0900

    Update frog_rsp.py
    컴퓨터가 랜덤으로 가위 바위 보 중 하나를 내는 함수 추가

--- frog_rsp.py
+++ frog_rsp.py
@@ -1,3 +1,9 @@
+import random
+
 class CPU_RSP: #컴퓨터의 값 처리
   def __init__ (self):
-    pass
+    self.choice_list = ['가위','바위','보']
+  
+  def choice (self):
+    computer_choice = random.choice(self.choice_list)
+    return computer_choice
